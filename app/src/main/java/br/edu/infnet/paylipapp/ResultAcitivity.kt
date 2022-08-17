ec951d8a305c6e8f1eb5a56a218725fd72aa5271
Cahcastroc <camila.castronunes@gmail.com>
2022-08-17 11:01:14 -0300

    visual-interface-mainActivity

---
+++ app/src/main/java/br/edu/infnet/paylipapp/ResultAcitivity.kt
@@ -0,0 +1,11 @@
+package br.edu.infnet.paylipapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultAcitivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result_acitivity)
+    }
+}
