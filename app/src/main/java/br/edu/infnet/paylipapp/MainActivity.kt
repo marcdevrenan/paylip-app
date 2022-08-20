8b24d1494966ee180e902a2012a8e74c7d5930ad
I_Igor Botani <igor.goncalves@mirante.net.br>
2022-08-20 14:06:51 -0300

    Add written to file

--- app/src/main/java/br/edu/infnet/paylipapp/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/paylipapp/MainActivity.kt
@@ -5,39 +5,56 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import java.io.BufferedWriter
+import java.io.FileOutputStream
+import java.io.LineNumberReader
+import java.io.OutputStreamWriter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btCalculate = findViewById<Button>(R.id.btNewSimulate)
         val etSalary = findViewById<EditText>(R.id.etSalary)
         val etDependents = findViewById<EditText>(R.id.etDependents)
         val etAlimony = findViewById<EditText>(R.id.etAlimony)
         val etOthers = findViewById<EditText>(R.id.etOthers)
 
-
-
+        val btCalculate = findViewById<Button>(R.id.btNewSimulate)
         btCalculate.setOnClickListener {
-            if(etSalary.toString().isNotBlank()) {
+            if (etSalary.toString().isNotBlank()) {
+                val salary = etSalary.text.toString()
+                val dependents = etDependents.text.toString()
+                val alimony = etAlimony.text.toString()
+                val others = etOthers.text.toString()
 
                 val intent = Intent(this, ResultAcitivity::class.java)
-                intent.putExtra("etSalary",etSalary.text.toString())
-                intent.putExtra("etDependents",etDependents.text.toString())
-                intent.putExtra("etAlimony",etAlimony.text.toString())
-                intent.putExtra("etOthers",etOthers.text.toString())
+                intent.putExtra("etSalary", salary)
+                intent.putExtra("etDependents", dependents)
+                intent.putExtra("etAlimony", alimony)
+                intent.putExtra("etOthers", others)
 
+                saveToFile(salary, dependents, alimony, others)
                 startActivity(intent)
 
-            }else{
+            } else {
                 getString(R.string.insira_o_valor_do_sal_rio_bruto)
             }
-
         }
-
-
     }
 
-
+    private fun saveToFile(salary: String, dependents: String, alimony: String, others: String) {
+        val fos: FileOutputStream = this.openFileOutput("dados-usuario.txt", MODE_PRIVATE)
+        val date = Date().toString()
+        val texto = "Salary: $salary\n" +
+                    "Dependents: $dependents\n" +
+                    "alimony: $alimony\n" +
+                    "Others: $others\n + " +
+                    "Date: $date"
+        fos.write(texto.toByteArray())
+        fos.close()
+    }
 }
+
