08a688427982b566032d1d0ecdb82cfbdf363e0e
Julia Paz <juliapazpinheiro@gmail.com>
2022-08-22 14:50:58 +0100

    permissions complete

--- app/src/main/java/br/edu/infnet/paylipapp/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/paylipapp/MainActivity.kt
@@ -17,9 +17,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import java.io.*
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.nio.file.StandardOpenOption
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -62,7 +59,6 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, "onCreate: Permission was not granted, request")
                     requestPermission()
                 }
-
             } else {
                 getString(R.string.insira_o_valor_do_sal_rio_bruto)
             }
@@ -71,34 +67,18 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun saveToFile(salary: String, dependents: String, alimony: String, others: String) {
-      var line = 0
-      var text : String? = null
+
+
               try {
                   val fos: FileOutputStream =
                       this.openFileOutput("dados-usuario.txt", MODE_PRIVATE)
-                 // val outputWriter = OutputStreamWriter(fos)
                   val date = Date().toString()
-
-
-                  text = "Salary: $salary\n" +
+                  var text = "Salary: $salary\n" +
                           "Dependents: $dependents\n" +
                           "alimony: $alimony\n" +
                           "Others: $others\n + " +
                           "Date: $date\n" +
                           "-------------------------------------------------\n"
-
-
-                      //fos.write(text.toByteArray())
-                  //if(line != 0){
-                    //  val fin = openFileInput("dados-usuario.txt")
-                    //  val inputStreamReader = InputStreamReader(fin)
-                    //  val bufferedReader = BufferedReader(inputStreamReader)
-                    //  val reader = bufferedReader
-                    //  reader.useLines {
-                    //      it.map { text -> "$text, $fin" }
-                    //      }}
-
-
                   val path = "/data/data/br.edu.infnet.paylipapp/files/dados-usuario.txt"
                   val data = text
                   try {
@@ -108,61 +88,6 @@ class MainActivity : AppCompatActivity() {
                   }catch (e: IOException) {
                       e.printStackTrace()
                   }
-
-
-
-
-
-                  //fos.close()
-                  //var contents = ""
-                  //val fin = openFileInput("dados-usuario.txt")
-                  //val inputStreamReader = InputStreamReader(fin)
-                  //val bufferedReader = BufferedReader(inputStreamReader)
-                  //val sb = StringBuilder()
-                 //var line: String?
-                  //while (bufferedReader.readLine().also { line = it} != null) {
-                  //    sb.append(line)
-
-                  //}
-                  //inputStreamReader.close()
-
-                  //text = contents + "\n" + text
-                  //var escrito = fos.write(text.toByteArray())
-                  //File("dados-usuario.txt").writeText(Iterable<String>.joinToString(), Charsets.UTF_8)
-
-                  //var extr:String = Environment.getExternalStorageDirectory().toString();
-                  //val mFolder = File(extr+"dados-usuario.txt")
-                  //val fileContent = this::class.java.getResource("dados-usuario.txt").readText()
-                  //val inputStream: FileInputStream = File("dados-usuario.txt").inputStream()
-                  //val inputString = inputStream.bufferedReader().use { it.readText()}
-                  //val fileContent = object {}.javaClass.getResource("/data/data/br.edu.infnet.paylipapp/files/dados-usuario.txt")?.readText();
-                    //val bufferedReader: BufferedReader = fos.bufferedReader()
-                    //val inputString = bufferedReader.use { it.readText() }
-
-                  //text = inputString + "\n" + text
-                  //fos.write(text.toByteArray())
-
-                 // if (lines > 0){
-                      //outputWriter.write(text)
-                      //FileOutputStream("dados-usuario.txt", true).bufferedWriter().use { writer ->
-                      //    text
-                    //  outputWriter.use { "data/data/br.edu.infnet.paylipapp/files/dados-usuario.txt" }
-                    //  outputWriter.append(text)
-                    //  FileOutputStream("dados-usuario.txt", true).use { writer ->
-                    //      "$text, $outputWriter"}
-                      //outputWriter.flush()
-                   //   ++lines
-                 // }
-                 // else{
-                   //   outputWriter.write(text)
-                    //  ++lines
-                  //}
-                  //var extr:String = Environment.getFilesDir("/data/data/br.edu.infnet.paylipapp/files/").toString();
-                  //val mFolder = File(extr+"dados-usuario.txt")
-                  //File(outputWriter.toString()).writeText(mFolder+"hello")
-
-                  //outputWriter.write(text)
-                  //outputWriter.close()
 
                    Toast.makeText(this, "Salvo", Toast.LENGTH_SHORT).show()
                } catch (e: FileNotFoundException) {
@@ -174,53 +99,7 @@ class MainActivity : AppCompatActivity() {
                   } catch (e: Exception) {
                       e.printStackTrace()
                   }
-
-
-        //val input = conn.inputStream
-        //val allText = input.bufferedReader().use(BufferedReader::readText)
-        //val result = StringBuilder()
-
-        //result.append(allText)
-        //return result.toString()
-//val fin = openFileInput("dados-usuario.txt")
-        //val inputStreamReader = InputStreamReader(fin)
-        //val bufferedReader = BufferedReader(inputStreamReader)
-        //val sb = StringBuilder()
-        //var line: String?
-        //while (bufferedReader.readLine().also { line = it} != null) {
-        //    sb.append(line)
-        //      try {
-      //          val fos: FileOutputStream =
-      //              this.openFileOutput("dados-usuario.txt", MODE_PRIVATE)
-      //          val date = Date().toString()
-      //          val texto = "Salary: $salary\n" +
-      //                  "Dependents: $dependents\n" +
-      //                  "alimony: $alimony\n" +
-      //                  "Others: $others\n + " +
-      //                  "Date: $date"
-      //          fos.write(texto.toByteArray())
-     //           fos.close()
-     //           Toast.makeText(this, "Salvo", Toast.LENGTH_SHORT).show()
-     //       } catch (e: FileNotFoundException) {
-     //           e.printStackTrace()
-     //       } catch (e: NumberFormatException) {
-     //           e.printStackTrace()
-   //         } catch (e: IOException) {
-  //              e.printStackTrace()
-  //          } catch (e: Exception) {
-  //              e.printStackTrace()
-  //          }
-
         }
-        //val fos: FileOutputStream = this.openFileOutput("dados-usuario.txt", MODE_PRIVATE)
-        //val date = Date().toString()
-        //val texto = "Salary: $salary\n" +
-        //            "Dependents: $dependents\n" +
-        //            "alimony: $alimony\n" +
-        //            "Others: $others\n + " +
-        //            "Date: $date"
-        //fos.write(texto.toByteArray())
-        //fos.close()
 
     private fun requestPermission(){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
@@ -241,7 +120,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
         else{
-            //Android is below 11(R)
             ActivityCompat.requestPermissions(this,
                 arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE),
                 STORAGE_PERMISSION_CODE
@@ -251,9 +129,8 @@ class MainActivity : AppCompatActivity() {
     private val storageActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(
     )){
         Log.d(TAG, "storageActivityResultLauncher: ")
-        //here we will handle the result of our intent
+        //here we will handle the result
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-            //Android is 11(R) or above
             if (Environment.isExternalStorageManager()){
                 //Manage External Storage Permission is granted
                 Log.d(TAG, "storageActivityResultLauncher: Manage External Storage Permission is granted")
@@ -271,17 +148,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
         else{
-            //Android is below 11(R)
         }
     }
 
     private fun checkPermission(): Boolean{
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
-            //Android is 11(R) or above
             Environment.isExternalStorageManager()
         }
         else{
-            //Android is below 11(R)
             val write = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
             val read = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
             write == PackageManager.PERMISSION_GRANTED && read == PackageManager.PERMISSION_GRANTED
@@ -313,7 +187,7 @@ class MainActivity : AppCompatActivity() {
                 else{
                     //External Storage Permission denied...
                     Log.d(TAG, "onRequestPermissionsResult: External Storage Permission denied...")
-                    Toast.makeText(this, "Permiss]ao negada", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show()
                 }
             }
         }
